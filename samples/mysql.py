32a7540d02304d2998b1c0fc88ce4dedc8aa45b0
youngji Jang <btg1631@naver.com>
2024-04-04 12:23:51 +0900

    PUSH

--- samples/mysql.py
+++ samples/mysql.py
@@ -12,32 +12,36 @@ def pose_problem():
     problem_type = input("문제 유형을 입력하세요. : ") # 4 지선다
     problem_count = input("문항 수를 입력하세요. : ")  # 5 문항
     
-    Question_id = 1
-    Choice_id = 1 
+    Question_id = 1     # QUESTION_ID
+    Choice_id = 1       # CHOICE_ID
 
-    for i in range(int(problem_type)):
-    
+    for i in range(int(problem_count)):
+        # 문제 INSERT
         print("문제와 선택지를 입력하세요 : ")
         Question_title = input("문항{} : ".format(i+1))
-        # 문제 INSERT
+
         sql = "INSERT INTO QUESTION (QUESTION_ID, QUESTION_TITLE) VALUES (%s, %s)"
         cursor.execute(sql, (Question_id, Question_title))
         conn.commit()
 
-        print("선택지 : ")
 
+        # 선택지 INSERT
+        print("선택지 : ")
         Choice_num = 1
-        for j in range(int(problem_count)):
+        for j in range(int(problem_type)):
             Choice_choice = input("{}. ".format(j+1))
-            # 선택지 INSERT
+        
             sql = "INSERT INTO CHOICE (CHOICE_ID, QUESTION_ID, CHOICE_CHOICE, CHOICE_NUM) VALUES (%s, %s, %s, %s)"
             cursor.execute(sql, (Choice_id, Question_id, Choice_choice, Choice_num))
             conn.commit()
-            Choice_id += 1
+            
+            Choice_id += 1       # CHOICE_ID
             Choice_num += 1
+
 
         Question_score = int(input("점수 : "))
         Question_answer = int(input("정답 : "))
+
 
         # 문제 점수 UPDATE
         sql = "UPDATE QUESTION SET QUESTION_SCORE = %s WHERE QUESTION_ID = %s"
@@ -51,75 +55,103 @@ def pose_problem():
 
         Question_id += 1
 
+def solving_problem():
+    User_id = 1        # USER_ID
+    Result_id = 1        # RESULT_ID
 
-try:
-    with conn.cursor() as cursor:
-        # 문제 내기        
-        # pose_problem()
-
-        User_id = 1
-        # 응시자 문제풀기
+    while True:
+        
+        # 응시자 이름 INSERT
         User_name = input("응시자 이름을 입력하세요: ")
+        
         sql = "INSERT INTO USER (USER_ID, USER_NAME) VALUES (%s, %s)"
         cursor.execute(sql, (User_id, User_name))
         conn.commit()
+
+        # 문제 풀기
         print("문제를 풀어주세요:")
 
-        
-        
+        # QUESTION_TITLE SELECT
         sql = "SELECT QUESTION_TITLE, QUESTION_ID FROM QUESTION"
         cursor.execute(sql)
         datas = cursor.fetchall()
+        num_count_q = 1       # 문항 번호COUNT
+        for data in datas:
+            print("문항{}: {}".format(num_count_q, data[0]))
+            Question_id = data[1]
 
-        for i in range(len(datas)):
-            for data in datas:
-                print("문항{}: {}".format(i+1, data[0]))  # 각 행 출력
-                Question_id = data[1]
-                
-                print("선택지: ")
-                sql = "SELECT CHOICE_CHOICE FROM CHOICE WHERE QUESTION_ID = %s"
-                cursor.execute(sql, (Question_id))
-                choices = cursor.fetchall()
+            
+            num_count_c = 1       # 선택지 번호 COUNT
+            print("선택지: ")
+            sql = "SELECT CHOICE_CHOICE FROM CHOICE WHERE QUESTION_ID = %s"
+            cursor.execute(sql, (Question_id))
+            choices = cursor.fetchall()
+            for choice in choices:
+                print("{}. {}".format(num_count_c, choice[0]))
+                num_count_c += 1       # 선택지 번호 COUNT
+            
+            
+            # 응시자 정답 입력 후 RESULT TABLE에 INSERT
+            User_choice = input("답 :")
+            sql = "INSERT INTO RESULT (RESULT_ID, USER_ID, QUESTION_ID, CHOICE_ID) VALUES (%s, (SELECT USER_ID FROM USER WHERE USER_ID = %s), %s, %s)"
+            cursor.execute(sql, (Result_id, User_id, Question_id, User_choice))
+            conn.commit()
 
-                for j in range(len(choices)):
-                    for choice in choices:
-                        print("{}. {}".format(j+1, choice[0]))  # 각 행 출력
-                    input("답 :")
+            num_count_q += 1       # 문항 번호COUNT
+            Result_id += 1        # RESULT_ID
+        User_id += 1        # USER_ID
+        
+        # 계속 진행 여부
+        user_end = input("다음 응시자가 있나요? (계속: c, 종료: x): ")
+        # x 입력 시 break
+        if user_end == 'x':
+            break
+            
+def result():
+
+    print("각 문항 정답 : ")
+    sql = "SELECT CHOICE_NUM FROM CHOICE WHERE CHOICE_CORRECT = '정답' ORDER BY QUESTION_ID"
+    cursor.execute(sql)
+    choices = cursor.fetchall()
+    for choice in choices:
+        print("{} ".format(choice[0]))
 
 
-            User_id += 1
+    # 응시자별 채점 결과
+    average = 0
+    print("응시자별 채점 결과: ")
+    sql = "SELECT USER.USER_NAME, USER.USER_ID, SUM(CASE WHEN CHOICE.CHOICE_CORRECT = '정답' THEN QUESTION.QUESTION_SCORE ELSE 0 END) AS TOTAL_SCORE FROM USER LEFT JOIN RESULT ON USER.USER_ID = RESULT.USER_ID LEFT JOIN CHOICE ON RESULT.QUESTION_ID = CHOICE.QUESTION_ID AND RESULT.CHOICE_ID = CHOICE.CHOICE_NUM AND CHOICE.CHOICE_CORRECT = '정답' LEFT JOIN QUESTION ON RESULT.QUESTION_ID = QUESTION.QUESTION_ID GROUP BY USER.USER_ID, USER.USER_NAME"
+    cursor.execute(sql)
+    users = cursor.fetchall()
+    for user in users:
+        print("{}: {}점".format(user[0], user[2]))
+        average += user[2]
 
+    print("과목 평균 점수: {}점".format(average//len(users)))
+
+    # RESULT테이블의 QUETSION_ID랑 CHOICE_ID 두개를 가져가.
+    # 이 두개로 CHOICE 테이블에서 QUETSION_ID랑 CHOICE_NUM 두개가 일치하는 것의
+    # CHOICE_CORRECT가 '정답'이면 QUESTION테이블의 QUESTION_SCORE의 숫자를 SUM 하는거야.
+    # '정답'이 아니라 NULL값이면 SUM에 포함 안시키고
+
+try:
+    with conn.cursor() as cursor:
+
+        # 문제 내기        
+        # pose_problem()
+        print("—--------------------------------------------------")
+
+        # 문제 풀기
+        # solving_problem()
+        print("—--------------------------------------------------")
+
+        # 채점 결과
+        result()
 
         # # Delete
         # sql = "DELETE FROM TableName WHERE pk_id=%s"
         # cursor.execute(sql, (1,))
         # conn.commit()
-
-
-
-# # 종료 여부 입력 function
-# def End(collection, collection1, collection2):           # hint collection 추가
-#     user_end = 'q'           # hint
-#     while True:
-#         # c 입력 시 Todos() 다시 실행
-#         if user_end == "c":
-#             print("")
-#             Todos(user_id, collection1, collection2)
-#         # q 입력 시 User_name() 실행 후 Todos() 다시 실행
-#         elif user_end == "q":
-#             print("")
-#             print("------------------------")
-#             user_id = User_name(collection)
-#             Todos(user_id, collection1, collection2)
-#         # x 입력 시 프로그램 종료
-#         else:
-#             break
-
-#         print("c, q, x 중 하나를 입력하세요.")
-#         user_end = input("진행 여부: ")
-
-#     print("------------------------")
-#     print("프로그램이 종료되었습니다.")
 
 finally:
     conn.close()
